7d7af1cebcafede7d50b5e987ca45a3eef516048
Chelsea <155999117+Chelseuhh@users.noreply.github.com>
2024-09-16 00:55:01 -0400

    Add 6

---
+++ csd-310/module-6/mysql_test.py
@@ -0,0 +1,15 @@
+# mysql
+
+import mysql.connector
+from mysql.connector import Error
+
+# Database configuration
+db_config = {
+    'user': 'root',               
+    'password': 'OctLor1814!',
+    'host':"localhost"
+    'database': 'movies'
+    'raise_on_warnings':True 
+}
+
+
